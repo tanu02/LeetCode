b0067fe5b2005d45e11c4ec58c200d62b1530d4f
tanu02 <tanubatra02@gmail.com>
2021-03-21 20:36:34 +0530

    LinkedList Card

---
+++ LinkedList/src/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,24 @@
+
+//https://leetcode.com/problems/intersection-of-two-linked-lists/
+public class IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    	//the only thing we are trying to achieve is to place both the nodes at a location
+		// from where their remaining list is of equal size.
+
+        // or I should say both should traverse equal distance.
+		// If a has a leap of 2 nodes make it move 2 nodes in b’s list 
+        if (headA == null || headB == null) return null;
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            if (p1.next == null && p2.next == null) return null;  // if there is no common point
+            p1 = p1.next == null ? headB : p1.next;
+            p2 = p2.next == null ? headA : p2.next;
+
+        }
+        return p1;
+
+    }
+}

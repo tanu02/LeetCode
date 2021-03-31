181150fb94e350de2568eb411002e328c133adf5
tanu02 <tanubatra02@gmail.com>
2021-03-31 20:14:15 +0530

    Sort | Recursion 2

---
+++ Recursion2/src/QuickSort.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+class QuickSort {
+
+    public int[] sortArray(int[] nums) {
+        sort(0, nums.length - 1, nums);
+        return nums;
+    }
+
+    void sort(int st, int end, int[] arr) {
+        if (st > end) return;
+
+        int part = partition(st, end, arr);
+        sort(st, part - 1, arr);
+        sort(part + 1, end, arr);
+    }
+
+    int partition(int st, int end, int[] arr) {
+        int pivotVal = arr[end];
+        int j = st;
+
+        for (int i = st; i < end; i++) {        //process elements before pivot
+            if (arr[i] < pivotVal)  swap(i, j++, arr);
+        }
+
+        swap(j, end, arr); //swap pivot
+        return j;
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
---
+++ Recursion2/src/SearchA2DMatrixII.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/search-a-2d-matrix-ii/
+class SearchA2DMatrixII {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+			return false;
+
+		int i = matrix.length - 1;
+		int j = 0;
+		int col = matrix[0].length;
+
+		while (i >= 0 && j < col) {
+
+			if (target > matrix[i][j])
+				j++;
+			else if (target < matrix[i][j])
+				i--;
+			else if (target == matrix[i][j])
+				return true;
+		}
+		return false;
+
+	}
+}

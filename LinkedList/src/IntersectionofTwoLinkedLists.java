c6bbb4482d4251ab2cb2cb4ff0f7f40315a4e499
tanu02 <tanubatra02@gmail.com>
2021-04-06 00:01:31 +0530

    commenting

--- LinkedList/src/IntersectionofTwoLinkedLists.java
+++ LinkedList/src/IntersectionofTwoLinkedLists.java
@@ -8,11 +8,11 @@ public class IntersectionofTwoLinkedLists {
 
         // or I should say both should traverse equal distance.
 		// If a has a leap of 2 nodes make it move 2 nodes in b’s list 
-        if (headA == null || headB == null) return null;
+        if (headA == null || headB == null) return null; // if 1 list does not exist return
         ListNode p1 = headA;
         ListNode p2 = headB;
 
-        while (p1 != p2) {
+        while (p1 != p2) { //after one rotation if they intersect - next element will be the intersection or they will meet at null
             if (p1.next == null && p2.next == null) return null;  // if there is no common point
             p1 = p1.next == null ? headB : p1.next;
             p2 = p2.next == null ? headA : p2.next;

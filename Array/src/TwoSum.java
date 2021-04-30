c42133fec6934ea10946940b109b7e54fbe1cb8b
tanu02 <tanubatra02@gmail.com>
2021-05-01 03:51:18 +0530

    Array : 2Sum

---
+++ Array/src/TwoSum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) return new int[0];
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[]{i, map.get(target - nums[i])};
+
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
---
+++ Revision/src/Syntax.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Syntax {
+
+    public  void mapSyntax(){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        if(map.containsKey(123)) {}
+
+
+    }
+}

463826634a084dff563d384b9c8abdec9c745d7e
tanu02 <tanubatra02@gmail.com>
2021-05-02 02:44:15 +0530

    Java 8

--- Revision/src/Java8.java
+++ Revision/src/Java8.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -5,11 +6,11 @@ import java.util.stream.Stream;
 public class Java8 {
     public void primitiveArrayToNonPrimitiveArray(int[] arr) {
         List<Integer> list = Arrays.stream(arr)
-                              .boxed()
-                              .collect(Collectors.toList());
+                .boxed()
+                .collect(Collectors.toList());
     }
 
-    public void primitiveArrayToNonPrimitiveList(int[][] arr) {
+    public void primitiveArrayToNonPrimitiveList(int[][] arr) { //2d array to list
         Integer[][] output = Stream.of(arr).
                 map(interval -> Arrays.stream(interval).boxed().toArray(Integer[]::new))
                 .toArray(Integer[][]::new);
@@ -24,16 +25,39 @@ public class Java8 {
     }
 
     public void sortListCollections(List<Integer> list) {
-      Collections.sort(list, (a, b) -> Integer.compare(a, b));
+        Collections.sort(list, (a, b) -> Integer.compare(a, b));
     }
 
-    public void listToArray(){
+    public void listToArray() {
         List<Integer> list = new ArrayList<>();
-        int size  = list.size(); //first get the fixed size instead of using list.size()
+        int size = list.size(); //first get the fixed size instead of using list.size()
         list.toArray(new Integer[size]);//{1,2}
 
         List<int[]> list2 = new ArrayList<>(); //{{}}
         int size1 = list2.size();
         int[][] output = list2.toArray(new int[size1][]);
     }
+
+    public void arrayToList() { //create array or have an array
+        int index = 0, left = 0, right = 1;
+        List<List<Integer>> output = new ArrayList<>();
+
+        List<Integer> ans = Arrays.asList(index, left, right); //output 3 indexes here primitive type
+    }
+
+    public void arrayToListObject() {
+        Integer[] arr = new Integer[]{};
+        List<Integer> list = Arrays.asList(arr);
+        //either create Integer type array
+        //else use stream and box the array
+    }
+
+    public void mapSyntax() {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        if (map.containsKey(123)) {
+        }
+
+    }
 }
+

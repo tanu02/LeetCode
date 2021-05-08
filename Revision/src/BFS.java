0f96db07e9da31112a1fd8fd07ed27f406f5cd56
tanu02 <tanubatra02@gmail.com>
2021-05-08 22:56:04 +0530

    Course Schedule + BFS

---
+++ Revision/src/BFS.java
@@ -0,0 +1,165 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class BFS {
+}
+//317. Shortest Distance from All Buildings
+//https://leetcode.com/problems/shortest-distance-from-all-buildings/
+class ShortestDistancefromAllBuildings317 {
+    int[][] reach;
+    int[][] dist;
+    int[][] grid;
+    int row;
+    int col;
+
+    List<int[]> directions = Arrays.asList(new int[] { 0, 1 }, new int[] { 1, 0 }, new int[] { 0, -1 },
+            new int[] { -1, 0 });
+
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        row = grid.length;
+        col = grid[0].length;
+
+        reach = new int[row][col];
+        dist = new int[row][col];
+        this.grid = grid;
+
+        int totalBuildings = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    bfs(i, j);
+                    totalBuildings++;
+                }
+
+            }
+        }
+
+        int minDis = Integer.MAX_VALUE;
+        for (int i = 0; i < row; i++) { //building which can be reached from all the buildings
+            for (int j = 0; j < col; j++) {
+                if (reach[i][j] == totalBuildings && dist[i][j] < minDis) { //reachable from all the buildings then check whose has min distance
+                    minDis = dist[i][j];
+                }
+
+            }
+        }
+
+        return minDis == Integer.MAX_VALUE ? -1 : minDis;
+    }
+
+    private void bfs(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] visited = new int[row][col];
+        queue.add(new int[] { i, j });
+        int dis = 0;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            dis++; // same level same distance
+            for (int m = 0; m < size; m++) { // level
+                int[] build = queue.poll();
+
+                for (int[] dir : directions) { // child
+                    int y = build[0] + dir[0];
+                    int x = build[1] + dir[1];
+
+                    if (isValid(visited, y, x)) { //building(1) which exists(!= length) and not visited (!= 2)
+                        queue.add(new int[] { y, x });
+                        visited[y][x] = 2;
+                        reach[y][x]++;
+                        dist[y][x] += dis;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private boolean isValid(int[][] visited, int i, int j) {
+        if (i < 0 || j < 0 || i == row || j == col || grid[i][j] == 2 ||
+                grid[i][j] == 1 || visited[i][j] == 2)
+            return false;
+
+        return true;
+    }
+}
+
+//-------
+
+
+class ShortestPathinBinaryMatrix1091 {
+    List<int[]> directions = Arrays.asList(
+            new int[]{0, 1},
+            new int[]{1, 1},
+            new int[]{1, 0},
+            new int[]{1, -1},
+            new int[]{0, -1},
+            new int[]{-1, -1},
+            new int[]{-1, 0},
+            new int[]{-1, 1}
+
+    );
+
+    int[][] visited;
+
+    public int shortestPathBinaryMatrix(int[][] grid) {//from start till end
+
+        if (grid == null || grid.length == 0 || grid[0].length == 0 || grid[0][0] == 1)
+            return -1;
+
+        visited = new int[grid.length][grid[0].length];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            count++;
+            for (int i = 0; i < size; i++) {
+                int[] cell = queue.poll();
+                int row = cell[0];
+                int col = cell[1];
+
+                if (row == grid.length - 1 && col == grid[0].length - 1)
+                    return count; // dest
+
+                visited[row][col] = 1; // mark visited
+                List<int[]> neighbours = getNeighbours(row, col, grid, visited);
+
+                for (int[] neighbour : neighbours) {
+
+                    queue.add(new int[]{neighbour[0], neighbour[1]});
+                }
+
+            }
+        }
+        return -1;
+    }
+
+    private List<int[]> getNeighbours(int i, int j, int[][] grid, int[][] visited) {
+        List<int[]> neighbours = new ArrayList<>();
+
+        for (int[] dir : directions) { // neighbours
+            int y = i + dir[0];
+            int x = j + dir[1];
+            if (isValid(y, x, grid)) {
+                neighbours.add(new int[]{y, x});
+            }
+        }
+        return neighbours;
+    }
+
+    private boolean isValid(int i, int j, int[][] grid) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == 1 || visited[i][j] == 1) // if not visited
+            return false;
+        return true;
+    }
+}
---
+++ Revision/src/TopoSort.java
@@ -0,0 +1,110 @@
+import java.util.LinkedList;
+import java.util.*;
+
+public class TopoSort {
+}
+
+
+class CourseSchedule207 {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        if (numCourses <= 0 || prerequisites == null) return true;
+
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+        int[] indegreeCount = new int[numCourses];
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int start = prerequisites[i][0];
+            int end = prerequisites[i][1];
+
+            List<Integer> list = adjMap.getOrDefault(end, new ArrayList<>());
+            list.add(start);
+            adjMap.put(end, list);
+
+            indegreeCount[start]++;                 //save dependent nodes indegree > 0
+
+        }
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegreeCount[i] == 0) queue.offer(i); //retrieve those nodes which have 0 indegree
+        }
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Integer node = queue.poll();
+                count++;
+                List<Integer> list = adjMap.get(node);
+
+                if (list != null) {
+                    for (int elem : list) {
+                        indegreeCount[elem]--;
+                        if (indegreeCount[elem] == 0) queue.add(elem);
+                    }
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
+
+//prerequisites should not be null or of 0 size
+//create adjMap
+//update indegrees
+//enqueue  all 0 indegree nodes
+//add node in sol, count++
+//reduce indegree of neighbors
+//enqueue neighbors if indegree = 0
+//count == numCourses return output
+//return empty array
+
+class CourseScheduleII210 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || numCourses <= 0)
+            return new int[numCourses];
+
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+        int[] indegreeCount = new int[numCourses];
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int start = prerequisites[i][0];
+            int end = prerequisites[i][1];
+
+            List<Integer> list = adjMap.getOrDefault(end, new ArrayList<>()); //{1,0} means 1 is dependent on 0 : 0 -> 1
+            list.add(start);                                                  // 0 - 1 in map
+            adjMap.put(end, list);
+            indegreeCount[start]++;                                           //{1, 0}      0->1       increase indegree of 1
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegreeCount[i] == 0)
+                queue.add(i); //it is possible due to cycle queue is empty so don't rely on queue size
+        }
+
+        int index = 0;
+        int[] output = new int[numCourses];
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Integer node = queue.poll();
+                output[index++] = node; //first update output then increment
+
+                List<Integer> list = adjMap.get(node); //hence after last loop index will be equal to length
+                if (list != null) {
+                    for (Integer elem : list) {
+                        indegreeCount[elem]--;
+                        if (indegreeCount[elem] == 0) queue.offer(elem);
+                    }
+                }
+            }
+        }
+        return index == numCourses ? output : new int[0];
+    }
+}

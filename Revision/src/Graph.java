e481ba1356e00cda66d34ea8c288169832d08bec
tanu02 <tanubatra02@gmail.com>
2021-05-10 02:51:12 +0530

    Connected Components count

--- Revision/src/Graph.java
+++ Revision/src/Graph.java
@@ -1,8 +1,56 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Graph {
 }
+
+class NumberofConnectedComponentsUndirectedGraph323 {
+    public int countComponents(int n, int[][] edges) {
+        if (n <= 0 || edges == null || edges.length == 0 || edges[0].length == 0) return n;
+
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for (int[] edge : edges) { // 0 - 1
+            List<Integer> list1 = adjMap.getOrDefault(edge[0], new ArrayList<>());
+            list1.add(edge[1]);
+            adjMap.put(edge[0], list1);
+
+            List<Integer> list2 = adjMap.getOrDefault(edge[1], new ArrayList<>());
+            list2.add(edge[0]);
+            adjMap.put(edge[1], list2);
+
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                dfs(i, visited, adjMap);
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private void dfs(int node, Set<Integer> visited, Map<Integer, List<Integer>> adjMap) {
+        if (visited.contains(node)) return;
+
+        visited.add(node);
+        if (adjMap.containsKey(node)) {
+            for (Integer next : adjMap.get(node)) {
+                dfs(next, visited, adjMap);
+            }
+        }
+    }
+}
+//adjMap non directed
+//dfs on keyset
+//dfs on connected nodes
+
+//visited then return
+//visit
+//dfs neighbours
+//there can be 5 nodes  and 1 edge hence loop  through  the nodes and not keyset
+
 
 //https://leetcode.com/problems/longest-consecutive-sequence/
 class LongestConsecutiveSequence128 {

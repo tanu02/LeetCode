6f626598ba3ef9b998089c7c99cd9186d8f405a6
tanu02 <tanubatra02@gmail.com>
2021-05-22 03:35:55 +0530

    DFS visited concept

--- Revision/src/DFS.java
+++ Revision/src/DFS.java
@@ -34,3 +34,85 @@ class NumberofIslands200 {
         }
     }
 }
+
+class WordSearch1 {
+    char[][] board;
+    int row, col;
+    String word;
+
+    int[] rows = {0, 0, -1, 1};
+    int[] cols = {1, -1, 0, 0};
+
+    public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.row = board.length;
+        this.col = board[0].length;
+        this.word = word;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (dfs(i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int index) {
+        if (index == this.word.length()) return true; //word length
+
+        if (i < 0 || i == row || j < 0 || j == col || board[i][j] != word.charAt(index) || board[i][j] == 1)
+            return false; //exceeded boundary looking for next char means word length not reached
+
+        board[i][j] = '1'; //visit
+
+        for (int d = 0; d < rows.length; d++) { //explore neighbors
+            if (dfs(i + rows[d], j + cols[d], index + 1)) return true; //if any direction has the answer
+        }
+
+        board[i][j] = word.charAt(index); //unvisit
+        return false; //every state says if you consider me there is no path
+    }
+}
+//O(N*3^L)
+
+//chill
+//word search
+//             n1                 n2 || n3   if any works return true
+//        n2          n3
+//                                return false [none worked]
+//
+
+//strict
+//bipartite,
+//              n1                 n2 && n3 if any did not work return false;
+//        n2          n3
+//                                return true [all worked]
+
+
+//When to mark a node visited?
+//                n1 -> n2 -> n3
+//                    <------
+//if you return back to n1 from n3 and start processing n2 - will give a loop
+// hence visit n1
+// Dijkstra, island problems, normal dfs
+
+//        n1 -> n2 -> n3
+//            <------
+//Euler path - every time we visit a node we remove an edge be revisiting same node
+//so no need to mark visited
+//we are removing the edges[graph is manipulated] unlike above problems
+
+
+
+
+
+
+
+
+
+
+
+
+
+

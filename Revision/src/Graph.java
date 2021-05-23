16fff948b5241836e687f82072ccb3ba8296244a
tanu02 <tanubatra02@gmail.com>
2021-05-23 11:26:50 +0530

    Clone graph

--- Revision/src/Graph.java
+++ Revision/src/Graph.java
@@ -88,3 +88,58 @@ class LongestConsecutiveSequence128 {
 //create a set of all the elements
 //if arr[I] is the smallest element count the sequence[set does not contain smaller element than arr[I]]
 //return max length
+
+
+//Different input in graph problems:
+//1) n + edges -> disconnected - loop n -> topo
+//2) only edges -> disconnected -> loop edges -> disjoint and find if maybe loop/components/connected[graph is tree]
+//3) Node containing edges given -> connected -> src node then loop adj edges -> clone graph
+//4) array index is node and value in index edges -> [[1,2][2,0],[0,1]]     0 - 1
+
+
+class CloneGraph133 {
+    Map<Node, Node> cloneMap;
+
+    public Node cloneGraph(Node node) {
+        cloneMap = new HashMap<>();
+        if (node == null) return null;
+        return dfs(node);
+    }
+
+    private Node dfs(Node node) {
+
+        if (cloneMap.containsKey(node)) return cloneMap.get(node);
+        Node node1 = new Node(node.val, new ArrayList<>());
+        cloneMap.put(node, node1);
+
+        for (Node next : node.neighbors) {
+            node1.neighbors.add(dfs(next));  //every state returns its clones value
+        }
+        return node1;
+    }
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+
+    }
+}
+
+                                                                   //       |  /
+                                                                   //        2

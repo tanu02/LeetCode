e454745a5c1b858e28620d8776e9d78f0ec5b201
tanu02 <tanubatra02@gmail.com>
2021-05-25 08:52:51 +0530

    Permutation

--- Revision/src/BackTrack.java
+++ Revision/src/BackTrack.java
@@ -170,8 +170,8 @@ class Permutations46 {
     public List<List<Integer>> permute(int[] nums) {
 
         outputList = new ArrayList<>();  //
-        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        //box primitive integer to Integer object
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList()); //box primitive integer to Integer object
+
         backTrack(0, list); //in permutation q we don't need doubly ll as we are just swapping values and not removing any links unlike combination where we appended in empty list
         return outputList;
 
@@ -217,53 +217,12 @@ class PermutationsII47 {
         for (int i = index; i < currList.size(); i++) {
 
             if (uniqueEntries.contains(currList.get(i)))
-                continue; //every state should only process unique entries
-            //as length is same after swap eventually same permutation will be generated
+                continue; //every state should only process unique entries as length is same after swap eventually same permutation will be generated
             uniqueEntries.add(currList.get(i));
+
             Collections.swap(currList, index, i);
             backTrack(index + 1, currList); //every state has a index which will be swapped with remaining elements
             Collections.swap(currList, index, i);
         }
     }
 }
-
-class WordSearch1 {
-    char[][] board;
-    int row, col;
-    String word;
-
-    int[] rows = {0, 0, -1, 1};
-    int[] cols = {1, -1, 0, 0};
-
-    public boolean exist(char[][] board, String word) {
-        this.board = board;
-        this.row = board.length;
-        this.col = board[0].length;
-        this.word = word;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (dfs(i, j, 0))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean dfs(int i, int j, int index) {
-        if (index == this.word.length()) return true; //word length
-
-        if (i < 0 || i == row || j < 0 || j == col || board[i][j] != word.charAt(index) || board[i][j] == 1)
-            return false; //exceeded boundary looking for next char means word length not reached
-
-        board[i][j] = '1'; //visit
-
-        for (int d = 0; d < rows.length; d++) { //explore neighbors
-            if (dfs(i + rows[d], j + cols[d], index + 1)) return true; //if any direction has the answer
-        }
-
-        board[i][j] = word.charAt(index); //unvisit
-        return false; //every state says if you consider me there is no path
-    }
-}
-//O(N*3^L)
